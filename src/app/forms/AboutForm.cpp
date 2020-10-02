005846b6cacc8336e0296d8639105ac4d26e93d8
Rafał Jelonek <71409580+rjelonek@users.noreply.github.com>
2020-10-02 21:17:17 +0200

    Add about form to project

---
+++ src/app/forms/AboutForm.cpp
@@ -0,0 +1,9 @@
+#include "ClangFormatPCH1.h"
+#pragma hdrstop
+
+#include "AboutForm.h"
+
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+
+__fastcall TaboutForm::TaboutForm(TComponent *Owner) : TForm(Owner) {}
---
+++ src/app/forms/AboutForm.h
@@ -0,0 +1,16 @@
+#ifndef AboutFormH
+#define AboutFormH
+
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+
+class TaboutForm : public TForm
+{
+	__published :					 // IDE-managed Components
+				  private :			 // User declarations
+							public : // User declarations
+									 __fastcall TaboutForm(TComponent *Owner);
+};
+#endif
