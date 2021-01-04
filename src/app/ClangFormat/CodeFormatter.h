96e8e052f78f73c4c3c6b37ca911be30e51ce39d
Rafał Jelonek <71409580+rjelonek@users.noreply.github.com>
2021-01-04 23:26:29 +0100

    Use wrapper in CodeFormatter

--- src/app/ClangFormat/CodeFormatter.h
+++ src/app/ClangFormat/CodeFormatter.h
@@ -5,6 +5,7 @@
 #include "Settings.h"
 #include "Message.h"
 #include <map>
+#include <boost/shared_ptr.hpp>
 
 namespace ClangFormat
 {
@@ -24,8 +25,6 @@ namespace ClangFormat
 
 		String GetText(_di_IOTASourceEditor sourceEditor, int &editorSize);
 		void SetText(_di_IOTASourceEditor sourceEditor, int editorSize, String textToSet);
-		bool FormatSource(const String &textToFormat, String &output, String &errorMessage, const String pathFile, String style, String fallbackStyle,
-						  unsigned int startLine, unsigned int endLine);
 		FormatResult FormatSource(_di_IOTASourceEditor sourceEditor, unsigned int startLine, unsigned int endLine);
 		void GetBookmarkList(_di_IOTASourceEditor sourceEditor, std::map<int, TOTACharPos> &bookmarkList);
 		void SetBookmarkList(_di_IOTASourceEditor sourceEditor, std::map<int, TOTACharPos> &bookmarkList);
